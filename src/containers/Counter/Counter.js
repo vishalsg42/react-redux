ec8b7c1e79247e10a2ff3655283ba9d6e1e00d71
Vishal gupta <vishalsg@prdxn.com>
2018-07-04 12:32:25 +0530

    redux

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
     state = {
@@ -11,34 +12,29 @@ class Counter extends Component {
     counterChangedHandler = ( action, value = 1   ) => {
         console.log('action',action);
         console.log('value',value);
-        
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-            default:
-                console.log('hello');
-            
-        }
+
     }
 
     render () {
         return (
+            
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={ this.props.onIncrementCounter } />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter }  />
                 <CounterControl label="Add 5" clicked={ this.props.onAdd }  />
                 <CounterControl label="Subtract 5" clicked={ this.props.onSubtract }  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store</button>
+                <ul onClick={this.props.onDeleteResult}>
+                    {
+                        this.props.storedResults.map( strResult => (
+                            
+                            <li key={strResult.id} onClick={()=> this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                        ))
+                    }
+                </ul>
+                <iframe src="https://www.ustream.tv/embed/23595968?html5ui" style={{border: 0}}  width="720" height="405"></iframe>
             </div>
         );
     }
@@ -48,16 +44,19 @@ const mapStatetoProps = state => {
   // console.log(state);
   
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     } ;
 };
 
 const mapDispatchtoProps = dispatch => {
     return {
-       onIncrementCounter: () => dispatch({type: 'Increment_Counter',value: 1}),
-       onDecrementCounter: () => dispatch({type: 'Decrement_Counter',value: 1}),
-       onAdd: () => dispatch({type: 'ADD',value: 5}),
-       onSubtract: () => dispatch({type: 'SUBTRACT',value: 5})
+       onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT ,value: 1}),
+       onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT,value: 1}),
+       onAdd: () => dispatch({type: actionTypes.ADD ,value: 5}),
+       onSubtract: () => dispatch({type: actionTypes.SUBTRACT ,value: 5}),
+       onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT }),
+       onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT ,resultId: id}),
     } 
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,26 +1,54 @@
+import * as  actionTypes from './actions';
+
 const initialState =  {
-    counter: 10
+    counter: 10,
+    results: []
 }
 
 const reducer = (state = initialState,actions)=>{
-    if(actions.type === 'Increment_Counter') {
+    
+    if(actions.type === actionTypes.INCREMENT) {
         return {
+            ...state,
             counter: state.counter + actions.value
         }
     }
-    if(actions.type === 'ADD') {
+    if(actions.type === actionTypes.ADD) {
         return {
+            ...state,
             counter: state.counter + actions.value
         }
     }
-    if(actions.type === 'Decrement_Counter') {
+    if(actions.type === actionTypes.DECREMENT) {
         return {
-            counter: state.counter + actions.value
-        }
-    }
-    if(actions.type === 'SUBTRACT') {
-        return {
+            ...state,
             counter: state.counter - actions.value
+        }
+    }
+    if(actions.type === actionTypes.SUBTRACT ) {
+        return {
+            ...state,
+            counter: state.counter - actions.value
+        }
+    }
+    if(actions.type === actionTypes.STORE_RESULT ) {
+        return {
+            ...state,
+            results: state.results.concat({id: new Date(), value: state.counter })
+        }
+    }
+
+    if(actions.type === actionTypes.DELETE_RESULT) {
+        
+        const updateArray = state.results.filter((result)=> {
+            
+            return result.id !== actions.resultId
+        } );
+        console.log(updateArray);
+        
+        return {
+            ...state,
+            results: updateArray
         }
     }
     return state;
